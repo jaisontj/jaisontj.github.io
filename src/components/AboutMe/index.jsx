30569ab5c8f863c82a861d441576a58011373680
Jaison Titus <jaisontj92@gmail.com>
2018-12-15 00:18:55 +0530

    Content update

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -7,7 +7,7 @@ class AboutMe extends React.Component {
       <div>
         <h1 className="about__title">About Me</h1>
         <p> I am a software engineer who has extensive experience in building front-end applications for mobile and the web. Off-late, I have been having the time of my life exploring Distributed Systems.</p>
-        <p> Although, I am not actively looking at freelancing at the moment, I am always up for a chat to discuss ideas or anything for that matter. Feel free to ping me on any of the communication platform provided on the left. </p>
+        <p> Although, I am not actively looking at freelancing at the moment, I am always up for a chat to discuss ideas or anything for that matter. Feel free to ping me on any of the communication platforms provided. </p>
       </div>
 
     )
